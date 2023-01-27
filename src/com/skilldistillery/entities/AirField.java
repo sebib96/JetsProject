6bba3a1681f69864efd2cd9818fab32a9ccaeb28
sebib96 <sebastiantellob96@hotmail.com>
2023-01-27 18:18:15 -0500

    initial commit

---
+++ src/com/skilldistillery/entities/AirField.java
@@ -0,0 +1,14 @@
+package com.skilldistillery.entities;
+
+import java.util.List;
+
+public class AirField {
+private List<Jet> jets;
+
+public void showFastestJet() {
+	
+	}
+public void showListOfJets() {
+	
+}
+}
