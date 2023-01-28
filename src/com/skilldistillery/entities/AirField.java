6fab4edc1a91922c2446a5a0f86587f285147749
sebib96 <sebastiantellob96@hotmail.com>
2023-01-28 18:09:25 -0500

    fixed add userJet

--- src/com/skilldistillery/entities/AirField.java
+++ src/com/skilldistillery/entities/AirField.java
@@ -5,6 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import com.skilldistillery.app.JetsApp;
 
@@ -96,4 +97,68 @@ public class AirField {
 		
 	}
 
-}
+	public void addUserJet(Scanner kb) {
+		System.out.println("Is the Jet to be added: ");
+		System.out.println("1. A Fighter Jet");
+		System.out.println("2. A Cargo Jet");
+		System.out.println("3. A Commercial Jet");
+		System.out.println("4. Quit");
+		int userChoice = kb.nextInt();
+		kb.nextLine();
+		boolean running = true;
+		do {
+			switch (userChoice) {
+			case 1:
+				System.out.println("Enter the model of the Jet");
+				String fighterModel = kb.next();
+				System.out.println("Enter the top speed of the Jet");
+				double fighterSpeed = kb.nextDouble();
+				kb.nextLine();
+				System.out.println("Enter the range of the Jet");
+				int fighterRange = kb.nextInt();
+				kb.nextLine();
+				System.out.println("Enter the price of the Jet");
+				long fighterPrice = kb.nextLong();
+				kb.nextLine();
+				FighterJet userFighterJet = new FighterJet(fighterModel, fighterSpeed, fighterRange, fighterPrice);
+				AirField airField = new AirField();
+				jetList.add(userFighterJet);
+				System.out.println(fighterModel + " has been added to the fleet");
+				running = false;
+				break;
+			case 2:
+				System.out.println("Enter the model of the Jet");
+				String cargoModel = kb.nextLine();
+				System.out.println("Enter the top speed of the Jet");
+				double cargoSpeed = kb.nextDouble();
+
+				kb.nextLine();
+				System.out.println("Enter the range of the Jet");
+				int cargoRange = kb.nextInt();
+				System.out.println("Enter the price of the Jet");
+				long cargoPrice = kb.nextLong();
+				CargoPlane userCargoPlane = new CargoPlane(cargoModel, cargoSpeed, cargoRange, cargoPrice);
+				System.out.println(cargoModel + " has been added to the fleet");
+				break;
+			case 3:
+				System.out.println("Enter the model of the Jet");
+				String commercialModel = kb.nextLine();
+				System.out.println("Enter the top speed of the Jet");
+				double commercialSpeed = kb.nextDouble();
+				System.out.println("Enter the range of the Jet");
+				int commercialRange = kb.nextInt();
+				System.out.println("Enter the price of the Jet");
+				long commercialPrice = kb.nextLong();
+				JetsImpl userCommercialJet = new JetsImpl(commercialModel, commercialSpeed, commercialRange,
+						commercialPrice);
+
+				System.out.println(commercialModel + " has been added to the fleet");
+				break;
+			case 4:
+				System.out.println("Returning you to the main menu...");
+				break;
+			}
+		} while (running);
+	}
+	}
+
